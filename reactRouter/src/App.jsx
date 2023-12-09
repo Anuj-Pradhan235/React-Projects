fda6ce9575c95eb58a384ce89c372c2fc7e9e1a5
Anuj-Pradhan235 <pradhananuj235@gmail.com>
2023-12-09 09:22:05 +0530

    Done

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Github, { githubInfoLoader } from './components/Github/Github'
+import User from './components/User/User'
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout />,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+
+      },{
+        path:"about",
+        element:<About />
+      },{
+        path:"contact",
+        element:<Contact />
+      },{
+        path:"github",
+        element:<Github />,
+        loader: githubInfoLoader
+      },{
+        path:"user/:userId",
+        element:<User />
+
+      }
+    ]
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <RouterProvider router={router} />
+  </React.StrictMode>,
+)
